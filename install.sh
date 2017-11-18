dd85d45de64070236455781ed2c450b3a62e79ed
valhakis <william.valhakis@gmail.com>
2017-11-18 15:18:59 +0200

    k

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ mkdir -p $HOME/.vim/backup
 # vim plug installation
 curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# nvm installation
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+
 # create dynamic links
 ln -s $HOME/mas/vim/init.vim $HOME/.vimrc
 ln -s $HOME/mas/home/.bashrc $HOME/.bashrc
